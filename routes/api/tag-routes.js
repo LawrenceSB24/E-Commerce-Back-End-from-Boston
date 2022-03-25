da31fdaee46e08f1a1811201a35392c6dbfb96e9
LawrenceSB24 <lawrs2022@gmail.com>
2022-03-25 17:54:15 -0400

    POST, GET, PUT, and DELETE for Tag table

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,71 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{model: Product}]
+  })
+  .then((dbTag) => {res.json(dbTag)})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json('Data not computed.');
+    });
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  Tag.findByPk({
+    where: {
+      tag_name: req.params.tag_name,
+      include: [{model: Product}]
+    }
+  })
+  .then((dbTag) => {res.json(dbTag)})
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json('Data not computed.');
+  });
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+  Tag.create(req.body)
+  .then((newDbTag) => {res.json(newDbTag)})
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json('Data not computed.');
+  });
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+  Tag.update(
+    {id: req.params.id},
+
+    {where: {id: req.params.id}}
+  )
+  .then((updatedDbTag) => {res.json(updatedDbTag)})
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json('Data not computed.');
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+  Tag.destroy(
+    {where: {id: req.params.id}}
+  )
+  .then((deletedDbTag) => {res.json(deletedDbTag)})
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json('Data not computed.');
+  });
+});
+
+module.exports = router;
